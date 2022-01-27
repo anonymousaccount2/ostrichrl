2629143c864c96a3fb44d10d29a00c1edd072f07
anonymousaccount2 <anonymouspapersubmission2022@gmail.com>
2022-01-27 18:47:22 +0100

    first commit

---
+++ setup.py
@@ -0,0 +1,13 @@
+import setuptools
+
+
+setuptools.setup(
+    name='ostrichrl',
+    description='OstrichRL',
+    url='https://github.com/anonymousaccount2/ostrichrl.git',
+    version='0.1.0',
+    install_requires=['dm_control', 'lxml', 'numpy'],
+    license='MIT',
+    python_requires='>=3.6',
+    keywords=['ostrichrl', 'musculoskeletal', 'biomechanics',
+              'reinforcement learning'])
